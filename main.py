08e86da28a20eec2784dd494faf12d2d87c53d0e
Ben Frey <spartanhooah@gmail.com>
2022-11-10 09:50:49 -0500

    edit the todos

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-prompt = "Enter a todo: "
 todos = []
 
 while True:
-    todo = input(prompt)
+    user_action = input("Type 'add', or 'show', 'edit', or 'q': ").strip()
 
-    if todo == 'q':
-        break
-
-    todos.append(todo)
-
-print(todos)
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            i = 1
+            for item in todos:
+                print("{}: {}".format(i, item))
+                i = i + 1
+        case 'edit':
+            number = int(input("Enter the number of the item to edit: ")) - 1
+            to_edit = todos[number]
+            new_todo = input("Enter a todo: ")
+            todos[number] = new_todo
+        case 'q':
+            break
