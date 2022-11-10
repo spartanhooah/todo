3c97b34923c874acf5cf0411c8e1024362f2b69a
Ben Frey <spartanhooah@gmail.com>
2022-11-10 10:05:53 -0500

    add feature to complete a todo

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 todos = []
 
+
+def show_todos():
+    for index, item in enumerate(todos):
+        print(f"{index + 1}: {item}")
+
+
 while True:
-    user_action = input("Type 'add', or 'show', 'edit', or 'q': ").strip()
+    user_action = input("Type 'add', or 'show', 'edit', 'complete', or 'q': ").strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            i = 1
-            for item in todos:
-                print("{}: {}".format(i, item))
-                i = i + 1
+            show_todos()
         case 'edit':
+            show_todos()
             number = int(input("Enter the number of the item to edit: ")) - 1
-            to_edit = todos[number]
             new_todo = input("Enter a todo: ")
             todos[number] = new_todo
+        case 'complete':
+            show_todos()
+            number = int(input("Enter the number of completed item: ")) - 1
+            todos.pop(number)
         case 'q':
             break
