8e72660019d4be8136bf059bf7cc145102504f9e
Ben Frey <spartanhooah@gmail.com>
2022-11-22 14:33:25 -0500

    add doc strings

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def show_todos(todos_list):
-    for index, item in enumerate(todos_list):
-        print(f"{index + 1}: {item.strip()}")
+    """ Given a list of todos, print out an enumerated list. """
+    for i, item in enumerate(todos_list):
+        print(f"{i + 1}: {item.strip()}")
 
 
 def get_int_from_user(prompt):
+    """ Prompt the user for input. Convert the input to an
+     integer, then subtract one from it (for 1-based indexing).
+     """
     return int(input(prompt)) - 1
 
 
