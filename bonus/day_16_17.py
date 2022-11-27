5d8ad1a49581b72d705b2a6ae4ec0e68c577eb88
Ben Frey <spartanhooah@gmail.com>
2022-11-27 15:25:55 -0500

    finish compression util

--- bonus/day_16_17.py
+++ bonus/day_16_17.py
@@ -1,20 +1,33 @@
 import PySimpleGUI as sg
+from zip_creator import make_archive
 
 file_select_label = sg.Text("Select files to compress:")
 file_select_input = sg.Input()
-file_select_button = sg.FilesBrowse("Choose")
-
+file_select_button = sg.FilesBrowse("Choose", key="files")
 
 directory_select_label = sg.Text("Select destination folder:")
 directory_select_input = sg.Input()
-directory_select_button = sg.FolderBrowse("Choose")
+directory_select_button = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+
+result_label = sg.Text(key="result")
 
 window = sg.Window("File Compressor",
                    layout=[[file_select_label, file_select_input, file_select_button],
                            [directory_select_label, directory_select_input, directory_select_button],
-                           [compress_button]])
+                           [compress_button, result_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+
+    window["result"].update(value="Compression successful")
+
+# window.read()
 window.close()
