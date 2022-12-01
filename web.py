7e6006ee739c9dac0fed2dbb6b93658f402d480b
Ben Frey <spartanhooah@gmail.com>
2022-11-30 20:55:37 -0500

    implement add to-do in web gui

--- web.py
+++ web.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
+todos = [todo.strip() for todo in functions.get_todos()]
+
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
 
 st.title("My Todo App")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    st.checkbox(todo, key=index)
 
 st.text_input(label="Add a todo",
               label_visibility="hidden",
-              placeholder="Add a new to-do...")
+              placeholder="Add a new to-do...",
+              on_change=add_todo,
+              key="new_todo")
+
+
