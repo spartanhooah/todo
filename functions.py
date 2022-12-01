efae76b69bf902a3561031c9cef7a4e09c42d0c7
Ben Frey <spartanhooah@gmail.com>
2022-11-30 20:23:30 -0500

    work around weird images problem

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 FILEPATH = "todos.txt"
 
 
@@ -24,3 +27,14 @@ def write_todos(todos, filepath=FILEPATH):
     """ Write the to-do items list in the  text file """
     with open(filepath, 'w') as file:
         file.writelines([todo + "\n" for todo in todos])
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
--- gui.py
+++ gui.py
@@ -1,5 +1,11 @@
 import functions
 import PySimpleGUI as sg
+import os
+
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 
 def get_stripped_todos():
@@ -8,7 +14,7 @@ def get_stripped_todos():
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button(image_source="add.png",
+add_button = sg.Button(image_source=functions.resource_path("add.png"),
                        bind_return_key=True,
                        tooltip="Add a to-do",
                        key="add")
@@ -17,7 +23,7 @@ list_box = sg.Listbox(values=get_stripped_todos(),
                       enable_events=True,
                       size=(45, 10))
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(image_source="complete.png",
+complete_button = sg.Button(image_source=functions.resource_path("complete.png"),
                             tooltip="Complete the selected to-do",
                             key="complete")
 exit_button = sg.Button("Exit")
@@ -68,7 +74,7 @@ while True:
             window["todo"].update(value="")
         case "Exit":
             break
-        case "todos": # update input box when clicking a todo
+        case "todos":  # update input box when clicking a todo
             window["todo"].update(value=values["todos"][0])
         case sg.WINDOW_CLOSED:
             break
